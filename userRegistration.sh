84ab31b25de838948da4a0959916ed636963914d
PratibhaMastud <karandepratibha999@gmail.com>
2020-10-26 13:28:22 +0530

    Added:Enter Password at least 1 Upper Case

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 #Author:Pratibha Mastud
-#5.Enter Pre-defined Password Rule No.1-Minimum 8 character
+#6.Enter Pre-defined Password Rule No.2-Minimum 8 character should have one Upper case.
 shopt -s extglob
-#pattern
-regex="^[a-z]{8,}$"
+
 #user input
 read -p "Enter Valid Pass - " pass
-#check pass invalid or not
-if [[ $pass =~ $regex ]]
+if [[ "${#pass} -ge 8 && "$pass" == *[A-Z] && "$pass" == *[a-z]* && "$pass" == *[0-9]* && "$pass" == *[!@#%^_-]" ]];
 then
         echo "Valid pass"
 else
