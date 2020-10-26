76659aa15cf2b2949bf3d916c7735e102c271de9
PratibhaMastud <karandepratibha999@gmail.com>
2020-10-26 14:20:18 +0530

    Added:Enter Password atleast 1 Special Character

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 #Author:Pratibha Mastud
-#7.Enter Pre-defined Password Rule No.3-Minimum 8 character should have 1 Numeric Number.
+#8.Enter Pre-defined Password Rule No.4-Minimum 8 character should have 1 Special Character.
+
 shopt -s extglob
 
 #user input
 read -p "Enter Valid Pass - " pass
-if [[ ${#pass} -ge 8 && "$pass" == *[a-z]* && "$pass" == *[0-9]* ]];
+
+if [[ ${#pass} -ge 8 && "$pass" == *[a-z]* && "$pass" == *[0-9]* && "$pass" == *[!@#$%^*]* ]];
 then
         echo "Valid pass"
 else
